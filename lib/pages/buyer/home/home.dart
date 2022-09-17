6e276629047c145367337b47762053957e22fe2e
bahrijar <muhammadbahrijar4@gmail.com>
2022-09-17 15:00:25 +0700

    add global widget app bar

--- lib/pages/buyer/home/home.dart
+++ lib/pages/buyer/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:bukafranchise/pages/buyer/brand/list_brand.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -17,6 +18,17 @@ class _HomePageState extends State<HomePage> {
         child: Stack(
           children: [
             Text('ini home page'),
+            SizedBox(
+              height: 24,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (builder) => const ListBrandPage()));
+                },
+                child: const Text("List Brand"))
           ],
         ),
       ),
