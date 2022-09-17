e9be8ccadcecf97afe278847d16288d1f7291007
bahrijar <muhammadbahrijar4@gmail.com>
2022-09-17 11:42:08 +0700

    add assets utils

---
+++ lib/utils/assets.dart
@@ -0,0 +1,18 @@
+class Assets {
+  // PATH
+  static const String _imagePath = "assets/img/";
+  static const String _iconPath = "assets/icons/";
+
+  // IMAGES
+  static const String logo = "${_imagePath}logo.png";
+
+  // ICONS
+  static const String icHome = "${_iconPath}home.svg";
+  static const String icHomeActive = "${_iconPath}home-active.svg";
+  static const String icOrder = "${_iconPath}order.svg";
+  static const String icOrderActive = "${_iconPath}order-active.svg";
+  static const String icProfile = "${_iconPath}profile.svg";
+  static const String icProfileActive = "${_iconPath}profile-active.svg";
+  static const String icHeart = "${_iconPath}heart.svg";
+  static const String icHeartActive = "${_iconPath}heart-active.png";
+}
